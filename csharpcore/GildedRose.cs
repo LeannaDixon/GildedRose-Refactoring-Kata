9958154b17050fb51998f98d9841fff4211c913b
LeannaDixon <leannapdixon@outlook.com>
2021-05-07 12:19:51 +0100

    Implemented simple Strategy pattern for Aged Brie.

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -1,14 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace csharpcore
 {
+    delegate void QualityStrategy(Item item);
+
     public class GildedRose
     {
         IList<Item> Items;
+
+        Dictionary<string, QualityStrategy> QualityStrategyHandler;
+
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
+
+            QualityStrategyHandler = new Dictionary<string, QualityStrategy>()
+            {
+                {"Aged Brie", UpdateBrieQuality }
+            };
+        }
+
+        private void UpdateBrieQuality(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality++;
+            }
         }
 
         public void UpdateSellIn(Item item)
@@ -21,44 +40,51 @@ namespace csharpcore
             foreach (Item item in Items.Where(item => item.Name != "Sulfuras, Hand of Ragnaros"))
             {
 
-                //if aged brie
+                if (QualityStrategyHandler.ContainsKey(item.Name))
+                {
+                    QualityStrategyHandler[item.Name](item);
+                }
+                else
+                {
+                    //if aged brie
+                    // quality < 50
                     //quality++ to max 50
 
-                //if backstage pass
+                    //if backstage pass
                     //if close
                     //if really close
                     //if expired
 
-                //else
+                    //else
                     //quality--
 
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.Quality > 0)
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        item.Quality--;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Quality > 0)
                         {
-                            if ((item.SellIn < 11 ) && (item.Quality < 50))
-                            {
-                                item.Quality++;
-                            }
-
-                            if ((item.SellIn < 6) && (item.Quality < 50))
-                            {
-                                item.Quality++;
-                                
-                            }
+                            item.Quality--;
                         }
                     }
+                    else
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality = item.Quality + 1;
+
+                            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                            {
+                                if ((item.SellIn < 11) && (item.Quality < 50))
+                                {
+                                    item.Quality++;
+                                }
+
+                                if ((item.SellIn < 6) && (item.Quality < 50))
+                                {
+                                    item.Quality++;
+                                }
+                            }
+                        }
+                    } 
                 }
 
                 UpdateSellIn(item);
