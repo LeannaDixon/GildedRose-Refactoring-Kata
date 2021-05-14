31ccbe186acff84fc17bc09572939d0caec3fb87
Kelly Obrazcova <kelly.obrazcova@gmail.com>
2021-05-14 11:48:29 +0100

    Partway through converting SellIn to strategy pattern

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -5,12 +5,14 @@ using System.Linq;
 namespace csharpcore
 {
     delegate void QualityStrategy(Item item);
+    delegate void SellInStrategy(Item item);
 
     public class GildedRose
     {
         IList<Item> Items;
 
         Dictionary<string, QualityStrategy> QualityStrategyHandler;
+        Dictionary<string, SellInStrategy> SellInStrategyHandler;
 
         public GildedRose(IList<Item> Items)
         {
@@ -21,6 +23,23 @@ namespace csharpcore
                 {"Aged Brie", UpdateBrieQuality },
                 {"Backstage passes to a TAFKAL80ETC concert" , UpdateBackstageQuality }
             };
+
+            SellInStrategyHandler = new Dictionary<string, SellInStrategy>()
+            {
+                {"Aged Brie", UpdateBrieSellIn },
+                {"Backstage passes to a TAFKAL80ETC concert" , UpdateBackstageSellIn }
+
+            };
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (Item item in Items.Where(item => item.Name != "Sulfuras, Hand of Ragnaros"))
+            {
+                UpdateItemQuality(item);
+                UpdateSellIn(item);
+                UpdateItemSellIn(item);
+            }
         }
 
         private void UpdateBackstageQuality(Item item)
@@ -54,36 +73,48 @@ namespace csharpcore
             item.SellIn--;
         }
 
-        public void UpdateQuality()
-        {
-            foreach (Item item in Items.Where(item => item.Name != "Sulfuras, Hand of Ragnaros"))
+       private static void UpdateItemSellIn(Item item) {
+            if (item.SellIn < 0)
             {
-                UpdateItemQuality(item);
-
-                UpdateSellIn(item);
-
-                /* Use Chain of responsibility here for the Update Sellin.
-                 * Tried on Friday 7 May 2021 with a failing test, Leanna reverted all changes
-                 * */
-                if (item.SellIn < 0)
+                if (item.Name == "Aged Brie")
                 {
-                    if (item.Name == "Aged Brie")
+                    if (item.Quality < 50)
                     {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality++;
-                        }
-                    }
-                    else if ((item.Name != "Backstage passes to a TAFKAL80ETC concert") && (item.Quality > 0))
-                    {
-                        item.Quality--;
-                    }
-                    else 
-                    {
-                        item.Quality = 0;
+                        item.Quality++;
                     }
                 }
+                else if ((item.Name != "Backstage passes to a TAFKAL80ETC concert") && (item.Quality > 0))
+                {
+                    item.Quality--;
+                }
+                else
+                {
+                    item.Quality = 0;
+                }
             }
+       }
+        private static void UpdateBrieSellIn(Item item)
+        {
+            if (item.SellIn < 0)
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality++;
+                }
+            }
+        }
+
+        private static void UpdateBackstageSellIn(Item item)
+        {
+            if (item.SellIn > 0)
+            {
+                item.SellIn = 0;
+            }
+        }
+
+        private static void UpdateDefaultSellIn(Item item)
+        {
+
         }
 
         private void UpdateItemQuality(Item item)
@@ -94,15 +125,34 @@ namespace csharpcore
             }
             else
             {
-                DefaultItemStrategy(item);
+                DefaultItemQualityStrategy(item);
             }
         }
 
-        private static void DefaultItemStrategy(Item item)
+        private void UpdateItemSellInHandler(Item item)
+        {
+            if (SellInStrategyHandler.ContainsKey(item.Name))
+            {
+                SellInStrategyHandler[item.Name](item);
+            }
+            else
+            {
+                DefaultItemSellInStrategy(item);
+            }
+        }
+
+        private static void DefaultItemQualityStrategy(Item item)
         {
             if (item.Quality > 0)
             {
                 item.Quality--;
+            }
+        }
+        private static void DefaultItemSellInStrategy(Item item)
+        {
+            if (item.SellIn > 0)
+            {
+                item.SellIn--;
             }
         }
     }
